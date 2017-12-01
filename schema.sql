5c5d9d938200b3290b18a91db505a547389386d8
Steven Cadwallader <steven.caddie@gmail.com>
2017-11-30 19:08:39 -0600

    Final Commit

--- schema.sql
+++ schema.sql
@@ -1,27 +1,37 @@
-DROP DATABASE IF EXISTS bamazon;
-CREATE DATABASE bamazon;
+CREATE DATABASE Bamazon_db;
 
-USE bamazon;
+USE Bamazon_db;
 
 CREATE TABLE products(
-  item_id INT AUTO_INCREMENT NOT NULL,
-  product_name VARCHAR(45) NOT NULL,
-  department_name VARCHAR(45) NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
-  stock_quantity INT(10) NOT NULL,
-  primary key(item_id)
+  id INT NOT NULL AUTO_INCREMENT,
+  ProductName VARCHAR(100) NOT NULL,
+  DepartmentName VARCHAR(100) NOT NULL,
+  Price DECIMAL(10,2) default 0,
+  StockQuantity INT default 0,
+  PRIMARY KEY(id)
 );
 
-SELECT * FROM products;
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Brooks Launch 2', 'Running Shoes', 99, 12);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Adidas Supernova Glide 8', 'Running Shoes', 129.95, 20);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Mizuno Wave Rider 18', 'Running Shoes', 119.95, 10);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Nike LUNARGLIDE 8', 'Running Shoes', 119.95, 33);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Nike Men\'s Running Tee', 'Apparel', 70, 10);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Nike Men\'s Running Tank', 'Apparel', 45, 12);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Nike Women\'s Running Tee', 'Apparel', 75, 40);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Nike Women\'s Running Tank', 'Apparel', 50, 3);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Hydration Belt', 'Accessories', 55, 4);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('Headlamp', 'Accessories', 25, 20);
+INSERT INTO products(ProductName, DepartmentName, Price, StockQuantity) VALUES ('GPS Watch', 'Electronics', 299.99, 2);
 
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("GTX 1080 ti", "Electronics", 769.00, 15),
-  ("Radeon RX 580", "Electronics", 449.99, 20),
-  ("Turkey", "Food and Drink", 45.95, 50),
-  ("Jeans", "Clothing", 50.00, 500),
-  ("Drill", "Home Improvement", 54.25, 35),
-  ("Skillet", "Cookware", 25.00, 65),
-  ("Deadpool", "Films", 20.00, 25),
-  ("Lord of the Rings", "Films", 25.50, 57),
-  ("Blender", "Appliances", 75.00, 35),
-  ("Bath Towels", "Home and Kitchen", 20.99, 2300);
+CREATE TABLE departments (
+  DepartmentId INT NOT NULL AUTO_INCREMENT,
+  DepartmentName VARCHAR(100) NOT NULL,
+  OverheadCost DECIMAL(10,2) NOT NULL,
+  TotalSales DECIMAL(10,2),
+  PRIMARY KEY(DepartmentId)
+);
+
+INSERT INTO departments(DepartmentName, OverheadCost) VALUES('Running Shoes', 500);
+INSERT INTO departments(DepartmentName, OverheadCost) VALUES('Apparel', 500);
+INSERT INTO departments(DepartmentName, OverheadCost) VALUES('Electronics', 500);
+INSERT INTO departments(DepartmentName, OverheadCost) VALUES('Accessories', 500);
